4f039e8ff7a2e0cb489d496e133e9318d89d3795
up2dul <up2dul@gmail.com>
2026-05-01 11:25:57 +0700

    feat(routes): setup tanstack query client

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import "../index.css";
@@ -6,6 +7,19 @@ export const Route = createRootRoute({
   component: RootComponent,
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
